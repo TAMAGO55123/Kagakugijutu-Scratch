25be12b9920721722c3bc646277fd2e60255e950
TAMAGO55123 <tamago.55123@gmail.com>
2024-04-27 14:09:28 +0900

    update

--- addlibrary.js
+++ addlibrary.js
@@ -3,7 +3,7 @@ let liblink=[
     'https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r',
     'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy',
     'https://code.jquery.com/jquery-3.7.1.min.js',
-    
+    'https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js'
 ];
 var jslibdata1='';
 liblink.forEach(function (item, index){
