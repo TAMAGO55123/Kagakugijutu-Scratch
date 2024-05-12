e20db9dfa453705ff882e1810d26b19b353c525a
TAMAGO55123 <147392915+TAMAGO55123@users.noreply.github.com>
2024-05-12 17:56:52 +0900

    ajax

--- get.js
+++ get.js
@@ -1,3 +1,15 @@
 function getScID(grade){
-    
+    $.ajax({
+        type: "GET",
+        url: "https://script.google.com/macros/s/AKfycbx03lTqQg5mazmCyq-SsPmVBQYHhtKdYFT3X1kkHdsZK7qX80gpfoOeK-4myVW9sGYi/exec",
+        data:{"grade":grade},
+        dataType:"json",
+        async:false
+    })
+    .done(function (json){
+        console.log(json);
+    })
+    .fail(function (){
+        console.error('エラーが発生しました');
+    })
 }
