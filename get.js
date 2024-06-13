c5618c2624c7443dd0fc5f4b664b0342e242b26d
TAMAGO55123 <147392915+TAMAGO55123@users.noreply.github.com>
2024-06-13 21:35:46 +0900

    update

--- get.js
+++ get.js
@@ -1,17 +1,6 @@
 function getScID(grade){
-    $.ajax({
-        type: "GET",
-        url: "https://script.google.com/macros/s/AKfycbzZ18jdzpAoMQw7q8Ie9z5ren9Y5QUdv9jAq27gvTTijmc8jLBfg0eomYZJF8JwEpof/exec",
-        data:{"grade":grade},
-        dataType:"json",
-        async:false
-    })
-    .done(function (json){
-        console.log(json);
-        return json
-    })
-    .fail(function (){
-        console.error('エラーが発生しました');
-        return null;
-    })
+    var jsondatagrade={};
+    $.getJSON(`https://yawara.tamago55.f5.si/json/grade${grade}.json`, (data) =>{
+        data=jsondatagrade;
+    });
 }
