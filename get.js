e5e5a5789daae6829ff800493249c79fe8d9e1b8
TAMAGO55123 <147392915+TAMAGO55123@users.noreply.github.com>
2024-06-27 14:23:04 +0900

    =のつけ忘れ

--- get.js
+++ get.js
@@ -3,7 +3,7 @@ function getScID(grade){
     
     var jsondatagrade;
     $.getJSON(`https://yawara.tamago55.f5.si/json/grade${grade}.json`, (data) =>{
-        jsondatagrade data;
+        jsondatagrade=data;
     });
     return jsondatagrade
 }
