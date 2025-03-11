8f773bf791ecd33d8626c9e96c909260b7226208
TAMAGO55123 <tamago.55123@gmail.com>
2025-03-12 02:09:07 +0900

    finaly update(?)

--- check.js
+++ check.js
@@ -5,7 +5,7 @@
  * @returns {Boolean}
  */
 function check(func,nofunc) {
-
+    //QRコードによって設定されたコードを読み込み、時間内か確認する
     if( document.cookie.indexOf('data') >= 0 || document.cookie.indexOf('kannri') >= 0 ){
         func();
         return true
